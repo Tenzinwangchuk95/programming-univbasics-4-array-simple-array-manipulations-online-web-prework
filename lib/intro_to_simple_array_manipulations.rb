d983a208553505a685aea9ddc8e2fe727b2e0880
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 01:00:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,7 @@ def using_pop (array)
     deleted_string = array.pop
 end
 
-def pop_with_args (array,)
+def pop_with_args (array)
     dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
     dog_breeds = dog_breeds.pop(2)
 end
