d471ca2fdef97a61fde5c4635529bbeb8e342d6a
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 01:43:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,7 +54,7 @@ end
 
 def using_delete (array) 
     instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    no_offense_steven = instructors.delete("Steven")
+    no_offense_steven = array.delete("Steven")
 end
 
 def using_delete_at (array)
