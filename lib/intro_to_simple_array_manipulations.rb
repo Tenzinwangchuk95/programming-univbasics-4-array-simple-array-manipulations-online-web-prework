99488410012fb316d6c1503fb54c1be1e5523896
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 01:05:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,6 +17,5 @@ end
 
 def pop_with_args (array)
     dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    small_dogs = dog_breeds.pop(2)
-    small_dogs
+    small_dogs = array.pop(2)
 end
