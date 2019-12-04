7bf68c8b645a92d73ad12f6fd365ab634022f8d5
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 00:50:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,15 @@ def using_pop (array)
     deleted_string = array.pop
 end
 
+def pop_with_args (array)
+    dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    small_dogs = dog_breeds.pop
+  end 
+  it "takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them" do 
+    expect(@small_dogs).to eq(["Chihuahua", "Shiba Inu"])
+  end 
+
+  it "decreases the length of the array by 2" do
+    expect(@dog_breeds.size).to eq(2)
+  end
+end
