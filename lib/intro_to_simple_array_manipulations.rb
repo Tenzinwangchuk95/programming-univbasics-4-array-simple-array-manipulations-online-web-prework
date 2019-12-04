dfff8924cc5c7ac24c0cc65c158beab6a0bbc318
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 00:32:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,4 +5,5 @@ def using_push (array, next_color)
 end
 
 def using_unshift(array, first_color)
-  
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  first_color +
