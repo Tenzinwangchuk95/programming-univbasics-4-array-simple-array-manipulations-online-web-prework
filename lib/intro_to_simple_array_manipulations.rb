d51e3df0a5c587fa798b781d2288d1551bccdd22
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 01:39:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,12 +52,3 @@ def using_flatten (array)
      flat_array = array.flatten
 end
 
-def using_delete (array, "Steven") 
-    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    no_offense_steven = instructors.delete("Steven")
-end
-
-def using_delete_at (array, 2)
-    famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-    deleted_robot = array.delete(2)
-end
