dfe5c9426ab4943e58b25a1b060d6f6c11bafe0d
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 01:15:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,12 +40,4 @@ def using_insert (array, element)
     list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     another_language = "Python"
     new_array = array.insert(element)
-  end
-
-  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-    expect(@new_array[4]).to eq(@another_language)
-  end
-
-  it "increases the length of the array" do 
-    expect(@new_array.length).to eq(8)
-  end
+end
