ea2c949e1dd2d73141dd3e86826402516aa25fd0
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-04 01:11:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,7 +33,7 @@ def shift_with_args(array)
 def using_concat (array1, array2)
     my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["sports cars", "flatiron school"]
-    all_my_favs = concat(@my_favorite_things, more_favs)
+    all_my_favs = @my_favorite_things more_favs.concat
   end
    it "takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first" do 
     expect(@all_my_favs).to eq(["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"])
